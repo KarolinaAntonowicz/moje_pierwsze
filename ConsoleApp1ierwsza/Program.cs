965bb32f02e6468a075aefee334aa2114cc09c49
szymonsso <szymonsso07@gmail.com>
2019-11-24 07:40:55 +0100

    Add menu of application

---
+++ ConsoleApp1ierwsza/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1ierwsza
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("1.Dodaj liczby");
+            Console.WriteLine("2.Odejmij liczby");
+            Console.WriteLine("3.Pomnóż liczby");
+            Console.WriteLine("4.Podziel liczby");
+
+
+
+        }
+    }
+}
