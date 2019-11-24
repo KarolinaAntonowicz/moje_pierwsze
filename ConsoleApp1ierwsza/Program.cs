26ff74c5ccffac3ec0ae25c8b543768682c897c8
szymonsso <szymonsso07@gmail.com>
2019-11-24 09:31:47 +0100

    Revolution in repo

--- ConsoleApp1ierwsza/Program.cs
+++ ConsoleApp1ierwsza/Program.cs
@@ -12,12 +12,15 @@ namespace ConsoleApp1ierwsza
         {
             while (true)
             {
+                Console.WriteLine("4.Podziel liczby");
+                Console.WriteLine("5. exit");
+                int menuOption = Convert.ToInt32(Console.ReadLine());
                 Console.WriteLine("1.Dodaj liczby");
                 Console.WriteLine("2.Odejmij liczby");
                 Console.WriteLine("3.Pomnóż liczby");
                 Console.WriteLine("4.Podziel liczby");
                 Console.WriteLine("5. Wyjście");
-                int menuOption = Convert.ToInt32(Console.ReadLine());
+                int a = Convert.ToInt32(Console.ReadLine());
             }
             
 
